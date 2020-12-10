3e6abdf1bd5d6926f7e3e06f14631743c138e9ef
JohnHeroku <75782665+JohnHeroku@users.noreply.github.com>
2020-12-10 20:18:02 +0800

    Update ry.sh

--- dockershc/ry.sh
+++ dockershc/ry.sh
@@ -1,37 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@JohnHeroku 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
-
-mixool
-/
-across
-3
-3563
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-across/dockershc/ura3y.sh
-@mixool
-mixool Rename vra3y.sh to ura3y.sh
-Latest commit 8c36744 on 20 Oct
- History
- 1 contributor
-37 lines (36 sloc)  1.2 KB
-  
 #!/bin/sh
 ## 用于https://github.com/mixool/dockershc项目安装运行v2ray的脚本
 
@@ -45,7 +11,7 @@ if [[ ! -f "/workerone" ]]; then
     [
         {
             "port": "3000","listen": "0.0.0.0","protocol": "vmess",
-            "settings": {"clients": [{"id": "3d48e6f3-a19e-4a69-b547-16040b9a63a3"}, "level": 1, "alterId": 64]},
+            "settings": {"clients": [{"id": "3d48e6f3-a19e-4a69-b547-16040b9a63a3"}],"level": 1, "alterId": 64},
             "streamSettings": {"network": "ws","wsSettings": {"path": "/ry"}}
         }
     ],
@@ -69,15 +35,3 @@ else
     # start 
     /workerone -config /config.json >/dev/null 2>&1
 fi
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
